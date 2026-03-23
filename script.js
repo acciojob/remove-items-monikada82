cff7a1c29e67a1f34a7952751032ad82ec8f2999
monikada82 <damonika8208@gmail.com>
2026-03-23 20:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeColor(){
+	const select=document.getElementById("colorSelect");
+	const selectIndex=sel.selectedIndex;
+
+	if(selectIndex!==-1){
+		select.remove(selectIndex);
+	}
+}
+	document.querySelector('input[type="button"]').addEventListener("click",remove);
